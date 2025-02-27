93293e38ad7662675e6c32ddf7151894acf6df0e
Brayan Pinzon <58951748+Stiven7-7@users.noreply.github.com>
2025-02-27 17:03:06 -0500

    feat: implement country layout with footer and enhance home page design

--- src/app/country/country.routes.ts
+++ src/app/country/country.routes.ts
@@ -1,18 +1,26 @@
 import { Routes } from '@angular/router';
+import { CountryLayoutComponent } from './layouts/CountryLayout/CountryLayout.component';
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
 
 export const contryRoutes: Routes = [
   {
     path: '',
-    component: ByCapitalPageComponent,
+    component: CountryLayoutComponent,
+    children: [
+      {
+        path: 'by-capital',
+        component: ByCapitalPageComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'by-capital',
+      }
+    ]
   },
   // {
   //   path: 'country',
   // },
-  {
-    path: '**',
-    redirectTo: '',
-  }
+
 ];
 
 export default contryRoutes;
