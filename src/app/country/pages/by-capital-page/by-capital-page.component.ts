991bc521fa772e58b9f5d52d0a5c50cbac0bdb0d
Brayan Pinzon <58951748+Stiven7-7@users.noreply.github.com>
2025-02-28 17:20:45 -0500

    feat: implement country search functionality with API integration and display country list

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-import { SearchInputComponent } from "../../components/search-input/search-input.component";
-import { ListComponent } from "../../components/list/list.component";
+import { Component, inject, signal } from '@angular/core';
+import { SearchInputComponent } from '../../components/search-input/search-input.component';
+import { ListComponent } from '../../components/list/list.component';
+import { CountryService } from '../../services/country.service';
+import { Country } from '../../interfaces/country.interface';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -9,8 +11,23 @@ import { ListComponent } from "../../components/list/list.component";
 })
 export class ByCapitalPageComponent {
 
-  onValue($event: string) {
-    console.log($event, 'onValue');
-  }
+  countryServices = inject(CountryService);
 
- }
+  isLoading = signal(false);
+  isError = signal<string|null>(null);
+  countries = signal<Country[]>([]);
+
+  onValue(query: string) {
+    if (this.isLoading()) return;
+
+    this.isLoading.set(true);
+    this.isError.set(null);
+
+    this.countryServices.searchByCapital(query)
+      .subscribe( countries => {
+        this.isLoading.set(false);
+        this.countries.set(countries);
+
+    })
+  }
+}
